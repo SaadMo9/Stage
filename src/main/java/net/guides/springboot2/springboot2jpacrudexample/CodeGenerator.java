add10a64e41a7e74b1fc81b4a3eefad2c3429043
saad <mounaime.saad@gmail.com>
2022-06-15 11:08:49 +0100

    Generate BarCode

---
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/CodeGenerator.java
@@ -0,0 +1,20 @@
+package net.guides.springboot2.springboot2jpacrudexample;
+
+import net.glxn.qrgen.javase.QRCode;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+public class CodeGenerator {
+    public static BufferedImage generateQRCodeImage(String barcodeText) throws Exception {
+        ByteArrayOutputStream stream = QRCode
+                .from(barcodeText)
+                .withSize(250, 250)
+                .stream();
+        ByteArrayInputStream bis = new ByteArrayInputStream(stream.toByteArray());
+
+        return ImageIO.read(bis);
+    }
+}
